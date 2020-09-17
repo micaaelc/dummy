fc01fa520e269ac8bcd93748b928cf8a9bcb6a2f
micaaelc <71446228+micaaelc@users.noreply.github.com>
2020-09-17 14:20:47 +0100

    hello.cs

---
+++ hello.cs
@@ -0,0 +1,23 @@
+using System;
+
+// Create a Hello Hills Road program in GitHub
+
+namespace Hello
+
+{
+
+  class HelloHillsRoad
+
+  {
+
+    static void Main()
+
+    {
+
+      System.WriteLine("Hello Hills Road!");
+
+    }
+
+  }
+
+}
